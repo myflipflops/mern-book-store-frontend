6a66b189ed99ca0aaf4fd4b23ab02091b6cf9afc
Parag Chaudhary <parag.chaudhary@example.com>
2025-06-08 22:44:23 +0530

    Update baseURL.js

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-    return "http://localhost:5000, https://parags-mern-project.vercel.app/"
+    return "http://localhost:5000, https://parags-mern-project4.vercel.app/"
 }
 
 export default getBaseUrl;
